c6da760c2a1417cd3aab483f5d2b25915764beb0
Eryk Czarnota <erykczarnota1@gmail.com>
2022-07-10 19:22:16 +0200

    mobile nav update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 const headerBtn = document.querySelector(".header__btn")
+const navToggle = document.querySelector('.nav__toggle')
+const menuBar = document.querySelector('.fa-bars')
+const closeBar = document.querySelector('.fa-x')
+const show = document.querySelector('.show')
+
+const showNav = () => {
+    navToggle.style.display = 'flex';
+}
+
+const closeNav = () => {
+    navToggle.style.display = 'none';
+}
+
+menuBar.addEventListener('click', showNav)
+closeBar.addEventListener('click', closeNav)
